956684919632e4893682a322b8df2e74151a39eb
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-02-12 15:27:43 -0300

    Update format.py

--- Aulas/Solyd/format.py
+++ Aulas/Solyd/format.py
@@ -2,5 +2,5 @@ jogador = "Ronaldinho"
 time = "RealMadrid"
 
 print(f'O {jogador}, pertencente ao time {time}')
-
-print('O {}, pertencente ao time {}' .format('Ronaldinho', 'RealMadrid'))
+print('')
+print('O {}, pertencente ao time {}' .format('Ronaldinho Gaucho', 'RealMadrid.'))
