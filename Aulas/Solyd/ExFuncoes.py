9d2ef7cd93926b720c1cbc5a2583648f9967b33e
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-02-12 15:22:50 -0300

    Update and rename Funcoes.py to ExFuncoes.py

---
+++ Aulas/Solyd/ExFuncoes.py
@@ -0,0 +1,8 @@
+n1 = int(input("Entre com o primeiro número: "))
+n2 = int(input("Entre com o segundo número: "))
+
+def soma (n1 , n2):
+    resp = n1 + n2
+    return resp
+
+print(soma(n1 , n2))
--- Aulas/Solyd/Funcoes.py
+++
@@ -1,7 +0,0 @@
-def soma (n1 , n2):
-    resp = n1 + n2
-    return resp
-
-retorno = (10 + 7)
-
-print (retorno)
