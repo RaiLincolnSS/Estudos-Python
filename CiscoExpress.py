9beb20f90ed7ce9a740787eeecef749b3127ce6a
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-09-01 11:25:28 -0300

    Update CiscoExpress.py

--- CiscoExpress.py
+++ CiscoExpress.py
@@ -1,4 +1,5 @@
 # https://edube.org/learn/python-essentials-1-prt/lab-operadores-e-expressoes
+# https://edube.org/learn/python-essentials-1-prt/lab-operadores-e-expressoes-1
 
 x =  # hardcode your test data here
 x = float(x)
@@ -34,4 +35,52 @@ Output esperado
 y = -1.0
 y = 3.0
 y = -9.0
+*/
+-------------------------------------------
+
+#Segundo codigo
+#x = float(input("Enter value for x: "))
+#
+# Write your code here.
+#
+#print("y =", y)
+
+
+/*
+Objetivos
+familiarizar-se com o conceito de números, operadores e operações aritméticas em Python;
+compreender a precedência e associatividade dos operadores Python, bem como o uso adequado de parêntesis.
+Cenário
+A sua tarefa é a de completar o código, de forma a avaliar a seguinte expressão:
+
+
+O resultado deve ser atribuído a y. Tenha cuidado - observe os operadores e mantenha as suas prioridades em mente. Não hesite em utilizar os parêntesis de que necessitar.
+
+Pode utilizar variáveis adicionais para encurtar a expressão (mas não é necessário). Teste o seu código com cuidado.
+
+
+Dados de Teste
+Input de amostra: 1
+
+Output esperado:
+
+y = 0.6000000000000001
+
+Input de amostra: 10
+
+Output esperado:
+
+y = 0.09901951266867294
+
+Input de amostra: 100
+
+Output esperado:
+
+y = 0.009999000199950014
+
+Input de amostra: -5
+
+Output esperado:
+
+y = -0.19258202567760344
 */
