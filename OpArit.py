d3fdd0d563560d255149ee239f8209daa15084a2
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-09-01 10:52:17 -0300

    Create OpArit.py

---
+++ OpArit.py
@@ -0,0 +1,10 @@
+a = 2 # input a float value for variable a here
+b = 5 # input a float value for variable b here
+
+print(a+b) # output the result of addition here
+print(a-b) # output the result of subtraction here
+print(a*b) # output the result of multiplication here
+print(a/b) # output the result of division here
+print(a//b) # output the result of division integer here
+
+print("\nThat's all, folks!")
